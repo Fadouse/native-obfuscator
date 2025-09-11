1e008dc791623dab1a460b38aefafad20f8f4cd3
Fadouse <83526586+Fadouse@users.noreply.github.com>
2025-09-11 22:40:29 +0800

    Guard loader lookup and JNI_OnLoad env initialization

--- obfuscator/src/main/resources/sources/native_jvm_output.cpp
+++ obfuscator/src/main/resources/sources/native_jvm_output.cpp
@@ -29,13 +29,40 @@ $register_code
         JNINativeMethod loader_methods[] = {
             { (char *) method_name, (char *) method_desc, (void *)&register_for_class }
         };
-        env->RegisterNatives(env->FindClass("$native_dir/Loader"), loader_methods, 1);
+
+        jclass class_loader_class = env->FindClass("java/lang/ClassLoader");
+        if (env->ExceptionCheck())
+            return;
+        jmethodID get_system_loader = env->GetStaticMethodID(class_loader_class, "getSystemClassLoader", "()Ljava/lang/ClassLoader;");
+        if (env->ExceptionCheck())
+            return;
+        jobject system_loader = env->CallStaticObjectMethod(class_loader_class, get_system_loader);
+        if (env->ExceptionCheck())
+            return;
+        jmethodID load_class = env->GetMethodID(class_loader_class, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+        if (env->ExceptionCheck())
+            return;
+        jstring loader_name = env->NewStringUTF("$native_dir.Loader");
+        jclass loader_class = (jclass) env->CallObjectMethod(system_loader, load_class, loader_name);
+        env->DeleteLocalRef(loader_name);
+        env->DeleteLocalRef(system_loader);
+        env->DeleteLocalRef(class_loader_class);
+        if (env->ExceptionCheck() || loader_class == nullptr)
+            return;
+        if (env->RegisterNatives(loader_class, loader_methods, 1) != JNI_OK || env->ExceptionCheck()) {
+            env->DeleteLocalRef(loader_class);
+            return;
+        }
+        env->DeleteLocalRef(loader_class);
     }
 }
 
 extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env = nullptr;
-    vm->GetEnv((void **)&env, JNI_VERSION_1_8);
+    if (vm->GetEnv((void **)&env, JNI_VERSION_1_8) != JNI_OK || env == nullptr)
+        return JNI_ERR;
     native_jvm::prepare_lib(env);
+    if (env->ExceptionCheck())
+        return JNI_ERR;
     return JNI_VERSION_1_8;
 }
