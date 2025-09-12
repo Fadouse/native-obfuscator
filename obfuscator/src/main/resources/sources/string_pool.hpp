d55bd4b40d0ca1b2f3b5881b22cc2857b5b1c0d2
Fadouse <83526586+Fadouse@users.noreply.github.com>
2025-09-12 23:01:29 +0800

    Decrypt strings on demand

--- obfuscator/src/main/resources/sources/string_pool.hpp
+++ obfuscator/src/main/resources/sources/string_pool.hpp
@@ -2,8 +2,12 @@
 
 #define STRING_POOL_HPP_GUARD
 
+#include <cstddef>
+
 namespace native_jvm::string_pool {
-    void decrypt_pool();
+    void decrypt_string(std::size_t offset, std::size_t len);
+    void encrypt_string(std::size_t offset, std::size_t len);
+    void clear_string(std::size_t offset, std::size_t len);
     char *get_pool();
 }
 
